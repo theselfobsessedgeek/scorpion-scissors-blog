b9952200732ab15f0fdb5a67de7837b6c4b3adec
theselfobsessedgeek <shubhamahlawatg@gmail.com>
2023-03-01 14:24:07 +0530

    Done adding Routes and the crad component finalized

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./pages/home/home.component";
+import {AboutComponent} from "./pages/about/about.component";
+import {BlogComponent} from "./pages/blog/blog.component";
+import {ContactComponent} from "./pages/contact/contact.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',redirectTo:'/blog',pathMatch:"full"},
+  {path:'home', component: HomeComponent},
+  {path:'about',component:AboutComponent},
+  {path:'blog',component:BlogComponent},
+  {path:'contactUs',component:ContactComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContentCardComponent } from './elements/content-card/content-card.component';
+import { FooterComponent } from './elements/footer/footer.component';
+import { HeaderComponent } from './elements/header/header.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { BlogComponent } from './pages/blog/blog.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContentCardComponent,
+    FooterComponent,
+    HeaderComponent,
+    HomeComponent,
+    AboutComponent,
+    BlogComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
